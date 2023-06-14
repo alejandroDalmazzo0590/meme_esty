0d78f1b3b60490ed3222da445ca88d4b889cc7d2
Alejandro Dalmazzo <alejandro.dalmazzo@gmail.com>
2023-06-14 02:14:28 -0300

    enviado

---
+++ src.js/btnComenta.js
@@ -0,0 +1,8 @@
+let btnUsuario=document.getElementById("btnUsuario"); 
+btnUsuario.addEventListener("click", login);
+
+login
+{
+    alert("usted no puede irse de vacaciones por que es POBRE");
+}
+
